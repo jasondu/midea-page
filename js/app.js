cedc519d18db28a4a287db910d851aecb965b703
jason <du>
2015-11-25 18:42:27 +0800

    update

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by duguangwei on 15/11/24.
+ */
+var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    // 如果需要前进后退按钮
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev'
+})
